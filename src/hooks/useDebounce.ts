077bf87adcb41335ce63978e1da5900f579ff46d
ingridflack <ingrid_flack@hotmail.com>
2024-08-22 21:09:11 -0300

    Add useDebounce

---
+++ src/hooks/useDebounce.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect, SetStateAction } from "react";
+
+export function useDebounce<T>(
+  initialValue: T,
+  time: number
+): [T, T, React.Dispatch<SetStateAction<T>>] {
+  const [value, setValue] = useState<T>(initialValue);
+  const [debouncedValue, setDebouncedValue] = useState<T>(initialValue);
+
+  useEffect(() => {
+    const debounce = setTimeout(() => {
+      setDebouncedValue(value);
+    }, time);
+
+    return () => {
+      clearTimeout(debounce);
+    };
+  }, [value, time]);
+
+  return [debouncedValue, value, setValue];
+}
